13720a448ab87cd481c28ff7ae9100d0b0b5ed47
Karwan Essmat Othman <karwan.essmat@outlook.com>
2023-07-18 23:18:45 +0300

    Fixed some bugs

--- NumberToKurdishWords/Extensions/Conversion.cs
+++ NumberToKurdishWords/Extensions/Conversion.cs
@@ -3,18 +3,15 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace NumberToKurdishWords.Extensions
 {
- public static  class Conversion
+    public static class Conversion
     {
         private static bool _isNumberOnBillion;
         private static bool _isNumberOnTrillion;
 
 
-
         public static string ToKurdishText(this object val)
         {
-           
-
-            var isNumber = long.TryParse(val.ToString(), out var  number);
+            var isNumber = long.TryParse(val.ToString(), out var number);
 
             if (!isNumber)
             {
@@ -87,7 +84,7 @@ namespace NumberToKurdishWords.Extensions
             {
                 if (words != " ")
                 {
-                      words += " و ";
+                    words += " و ";
                 }
 
                 var unitsMap = new[] { "سفر", "یەک", "دوو", "سێ", "چوار", "پێنج", "شەش", "حەوت", "هەشت", "نۆ", "دە‌", "یازدە‌", "دوازدە‌", "سێزدە‌", "چواردە‌", "پازدە‌", "شازدە‌", "حەڤدە‌", "هەژدە‌", "نۆزدە‌" };
@@ -124,17 +121,12 @@ namespace NumberToKurdishWords.Extensions
             words = words.Replace("و ملیۆنێک", "و یەک ملیۆن");
             words = words.Replace("و ملیارێک", "و یەک ملیار");
 
-            if (_isNumberOnTrillion)
-            {
-                words = words.Replace("سەد ملیار", "سەد و یەک ملیار");
-                words = words.Replace("و تریلیۆنێک", "و یەک تریلیۆن");
+            if (!_isNumberOnTrillion) return words;
+            words = words.Replace("سەد ملیار", "سەد و یەک ملیار");
+            words = words.Replace("و تریلیۆنێک", "و یەک تریلیۆن");
 
-            }
-            
 
             return words;
         }
-
     }
-
 }
